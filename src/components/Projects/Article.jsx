17cd451b7730bec85348f9ae6b66371a3df4174b
Carole Gérard <gerard.carole78@gmail.com>
2024-01-19 09:34:46 +0100

    transition (article)

--- src/components/Projects/Article.jsx
+++ src/components/Projects/Article.jsx
@@ -4,17 +4,17 @@ import styles from "./Article.module.css";
 import { Link } from "react-router-dom";
 
 
-export default function Article({dataArticle}) {
+export default function Article({ dataArticle }) {
     return (
         <>
-            <article className={styles.articleContent}>
-                <h3>{dataArticle.title}</h3>
-                <Link to={dataArticle.to} target="_blank">
+            <Link to={dataArticle.to} target="_blank">
+                <article className={styles.articleContent}>
+                    <h3>{dataArticle.title}</h3>
                     <img src={dataArticle.src} alt={dataArticle.alt} />
-                </Link>
-                <h4>{dataArticle.project}</h4>
-                <p>{dataArticle.desc}</p>
-            </article>
+                    <h4>{dataArticle.project}</h4>
+                    <p>{dataArticle.desc}</p>
+                </article>
+            </Link>
         </>
     );
 };
