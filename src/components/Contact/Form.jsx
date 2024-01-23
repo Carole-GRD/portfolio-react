05a250654c5429516008865a32ffc3b516ba27a5
Carole Gérard <gerard.carole78@gmail.com>
2024-01-23 11:55:15 +0100

    Form

---
+++ src/components/Contact/Form.jsx
@@ -0,0 +1,53 @@
+import styles from "./Form.module.css";
+
+
+export default function Form() {
+    return (
+        <form action="">
+            <div className={styles.sections}>
+                <section className={styles.left}>
+                    <article>
+                        <label htmlFor="gender">Mr / Mme / Mlle :</label>
+                        <select name="gender" id="gender" defaultValue="">
+                            {/* <option value selected="selected"></option> */}
+                            <option value="" disabled hidden></option>
+                            <option value="madame">Madame</option>
+                            <option value="monsieur">Monsieur</option>
+                            <option value="mademoilselle">Mademoiselle</option>
+                        </select>
+                    </article>
+                    <article>
+                        <label htmlFor="lastName">Nom :</label>
+                        <input type="text" name="lastName" id="lastName" placeholder="Nom" />
+                    </article>
+
+                    <article>
+                        <label htmlFor="firstName">Prénom :</label>
+                        <input type="text" name="firstName" id="firstName" placeholder="Prénom" />
+                    </article>
+
+                    <article>
+                        <label htmlFor="tel">Téléphone :</label>
+                        <input type="tel" name="tel" id="tel" placeholder="Téléphone" />
+                    </article>
+                </section>
+
+                <section className={styles.right}>
+                    <article>
+                        <label htmlFor="email">Email :</label>
+                        <input type="email" name="email" id="email" placeholder="Email" />
+                    </article>
+
+                    <article>
+                        <label htmlFor="message">Message :</label>
+                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                    </article>
+                </section>
+            </div>
+
+            {/* <input type="submit" name="sendMessage" id="sendMessage" className={styles.sendMessage} value="Envoyer" /> */}
+            <button type="submit" name="sendMessage" id="sendMessage" className={styles.sendMessage}>Envoyer</button>
+
+        </form>
+    );
+};
